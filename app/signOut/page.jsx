cbc9c414f451b5ebc85ea640e79167a8c695e89f
Nkosinathi Milanzi <nathilanziv@gmail.com>
2024-10-09 23:51:37 +0200

    create sign out page with logic

---
+++ app/signOut/page.jsx
@@ -0,0 +1,29 @@
+// app/signOut/page.jsx
+"use client";
+
+import { useEffect } from 'react';
+import { logOut } from '../auhtentication';// Imported log-out function
+import { useRouter } from 'next/navigation';
+
+export default function SignOutPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleSignOut = async () => {
+      try {
+        await logOut();
+        router.push('/');
+      } catch (error) {
+        console.error("Error signing out:", error);
+      }
+    };
+    handleSignOut();
+  }, [router]);
+
+  return (
+    <div>
+      <h1>Signing Out...</h1>
+      <p>Please wait...</p>
+    </div>
+  );
+}
