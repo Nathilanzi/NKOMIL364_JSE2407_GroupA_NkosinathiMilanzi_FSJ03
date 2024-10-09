9271ad2b6c8ead10372b552f0a7393322c68c7d6
Nkosinathi Milanzi <nathilanziv@gmail.com>
2024-10-09 23:51:17 +0200

    create sign up page with logic

---
+++ app/signUp/page.jsx
@@ -0,0 +1,45 @@
+// app/signUp/page.jsx
+"use client";
+
+import { useState } from 'react';
+import { signUp } from '../auhtentication'; // Imported sign-up function
+import { useRouter } from 'next/navigation';
+
+export default function SignUp() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  const router = useRouter();
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    try {
+      await signUp(email, password);
+      router.push('/');
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <form onSubmit={handleSignUp}>
+        <input 
+          type="email" 
+          placeholder="Email" 
+          value={email} 
+          onChange={(e) => setEmail(e.target.value)} 
+        />
+        <input 
+          type="password" 
+          placeholder="Password" 
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)} 
+        />
+        <button type="submit">Sign Up</button>
+        {error && <p>{error}</p>}
+      </form>
+    </div>
+  );
+}
